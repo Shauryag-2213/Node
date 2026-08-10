7ef7766faa6bff4bc45f7e5cf7e833a7cf9cd660
Shauryag-2213 <shauryagoel2005@gmail.com>
2026-08-10 13:08:44 +0530

    Installed a new package named as Nodemon, and learned about path paramaeters and query strings.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
-console.dir(app);
+
 let port = 8080;
+
 app.listen(port, () => {     //app.listen is used to listen to the request send to the server by the client//
     console.log(`App is listening to the port${port}`);
 })
 app.get("/", (req,res) => { //.get is used to provide different responses for different paths
-    res.send("You reached at the root path");
+    res.send("Welcome to the Root Path");
 });
 app.get("/login", (req,res) => {
     res.send("You reached at the login path");
@@ -14,12 +15,24 @@ app.get("/login", (req,res) => {
 app.get("/home", (req,res) => {
     res.send("You reached at the Home path");
 });
-app.get("*", (req,res) =>{
+app.get("/:username/:id", (req,res) => {
+    let {username , id} = req.params;
+    let htmlStr = `<center><h1>Welcome to the page of ${username} along with the id ${id}</h1></center>`
+    res.send(htmlStr);
+});
+app.get("/search", (req,res) => {
+    let {q} = req.query;
+    if(!q){
+        res.send(`<h1>Nothing Searched</h1>`)
+    }
+    else{
+        res.send(`<h1>Results for searched query : ${q}</h1>`);
+    }
+});
+app.get("/*splat", (req,res) =>{
     res.send("This path does not exist");
 })
-app.post("/", (req,res) => {
-    res.send("You sent a post request to the root path.");
-});
+
 // app.use((req,res) => { //app.use is used to receive the request of the client and respond it to the client//
 //     // console.log(req); this determines that express can even represent objects unlike other languages
 //     console.log("Request Received");
