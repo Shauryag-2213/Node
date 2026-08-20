fdf11b8defc45db69aa8863ab569ca9449ac5b39
Shauryag-2213 <shauryagoel2005@gmail.com>
2026-08-21 01:53:18 +0530

    Learned Show Routing

--- EJS/index2.js
+++ EJS/index2.js
@@ -11,21 +11,30 @@ app.set("views", path.join(__dirname , "views"));
 app.use(express.static(path.join(__dirname , "public")));
 
 let posts = [
-{    
+{   
+    id : "1a",
     username : "Shaurya",
     content : "REST Class"
 },
 {
+    id : "2b",
     username : "Anubha",
     content : "Mother"
 },
 {
+    id : "3c",
     username : "Sanjay",
     content : "Father"
 }
 ]
 app.get("/posts", (req,res) => {
     res.render("index.ejs" , {posts});
+})
+
+app.get("/posts/:id", (req,res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id)
+    res.render("show.ejs", {posts});
 })
 
 app.get("/posts/new", (req,res) => {
